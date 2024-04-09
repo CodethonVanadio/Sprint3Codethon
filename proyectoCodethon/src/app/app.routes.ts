c21f74ba0ff22297d280a17eb3c52ffae3c8cedf
edgar91cuba <147478120+edgar91cuba@users.noreply.github.com>
2024-04-09 09:44:57 +0200

    Add files via upload

---
+++ proyectoCodethon/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+
+export const routes: Routes = [{
+path: '',
+component: HomeComponent
+
+},{
+path:'map',
+component: MapComponent}];
