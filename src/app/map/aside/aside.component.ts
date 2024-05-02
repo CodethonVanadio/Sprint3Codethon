d5d78ba4b5ed31fcb994363caacf7c9832cf9ac0
Carles Sánchez Moreno <119260678+CarlesNero@users.noreply.github.com>
2024-05-02 10:51:41 +0200

    ngFor

--- src/app/map/aside/aside.component.ts
+++ src/app/map/aside/aside.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MapComponent } from '../map.component';
-
-
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [MapComponent],
+  imports: [MapComponent, NgFor],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css',
 })
-
 export class AsideComponent {
   @Input() longitud: any = '';
   @Input() latitud: any = '';
+  @Input() nombreCalle: any = '';
+  @Input() routes: any = '';
 }
-
-
