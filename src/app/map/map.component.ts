098f23230b4fc1a9b3c86e481b609d802d75602f
CarlesNero <csanchezdsgn@gmail.com>
2024-05-07 23:45:19 +0200

    markers fixed, conexion back

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -40,14 +40,14 @@ export class MapComponent implements OnInit {
   map: any;
   longitud: any = 0;
   latitud: any = 0;
-  longitud2: any = -3.9249;
-  latitud2: any = -3.9249;
+  longitud2: any = 0;
+  latitud2: any = 0;
   taxiIcon: any;
   markers: Marker[] = [];
   marker: any = L.marker([0, 0]);
   searchLocation: any;
   nombreCalle: any;
-  routes: any;
+  routes: any[] = [];
   chargingStations: any[] = [];
   use: any = 0;
 
@@ -57,58 +57,50 @@ export class MapComponent implements OnInit {
     this.initMap();
   }
 
-  fetchPosts() {
-    const bbox = `(${this.latitud},${this.longitud}),(${this.latitud2},${this.longitud2})`;
-    const url = `https://api.openchargemap.io/v3/poi/?client=ocm.app.ionic.8.6.1&verbose=false&output=json&includecomments=true&maxresults=40&compact=true&boundingbox=(${this.latitud},${this.longitud}),(${this.latitud2},${this.longitud2})&key=53f3079e-75c6-40eb-bc30-8b8792c9602f`;
+  fetchPosts(latOrig: any, lngOrig: any, latDes: any, lngDes: any) {
+    const bbox = `${latOrig},${lngOrig}),(${latDes},${lngDes}`;
+    const url = `http://localhost:8989/saludo`;
+    const urlWithParams = url.concat(
+      `?latitudOrigen=${latOrig}
+      &longitudOrigen=${lngOrig}
+      &latitudDestino=${latDes}
+      &longitudDestino=${lngDes}`
+    );
+    // const url = `https://api.openchargemap.io/v3/poi/?client=ocm.app.ionic.8.6.1&verbose=false&output=json&includecomments=true&maxresults=40&compact=true&boundingbox=(${bbox})&key=53f3079e-75c6-40eb-bc30-8b8792c9602f`;
 
     axios
-      .get(url)
+      .get(urlWithParams)
       .then((response) => {
-        console.log(response.data);
-        this.chargingStations = response.data;
+        const stations = response.data;
+
+        stations.map((station: any) => {
+          const iconCharger = L.icon({
+            iconUrl: '../../../assets/images/charging-station.png',
+            iconSize: [40, 40],
+            iconAnchor: [16, 16],
+            popupAnchor: [0, -16],
+          });
+
+          L.marker([station.latitud, station.longitud], {
+            icon: iconCharger,
+            draggable: false,
+          })
+            .addTo(this.map)
+            .bindPopup(
+              `<div class="infoCharger" style="color: red">
+                            <h3>Nombre :</h3> ${station.nombre}
+                            <h3>Coste de uso:</h3> ${
+                              station.precio ? station.precio : 'desconocido'
+                            }
+                            <h3>Dirección:</h3> ${station.direccion}
+                        </div>`
+            )
+            .openPopup();
+        });
       })
       .catch((error) => {
         console.error('Error fetching posts:', error);
       });
-
-    this.chargingStations.forEach((station: any) => {
-      let use: string;
-      if (
-        station.UsageCost === '' ||
-        station.UsageCost === undefined ||
-        station.UsageCost === 'Desconocido'
-      ) {
-        use = 'Coste desconocido';
-      } else {
-        use = station.UsageCost;
-      }
-
-      console.log(station.AddressInfo.Latitude);
-      console.log(station.AddressInfo.Longitude);
-
-      const iconCharger = L.icon({
-        iconUrl: '../../../assets/images/charging-station.png',
-        iconSize: [40, 40],
-        iconAnchor: [16, 16],
-        popupAnchor: [0, -16],
-      });
-
-      L.marker([station.AddressInfo.Latitude, station.AddressInfo.Longitude], {
-        icon: iconCharger,
-        draggable: true,
-      })
-        .addTo(this.map)
-        .bindPopup(
-          `<div class="infoCharger" style="color: red">
-                        <h3>Nombre :</h3> ${station.AddressInfo.Title}
-                        <h3>Coste de uso:</h3> ${use}
-                        <h3>Dirección:</h3> ${station.AddressInfo.AddressLine1}
-                    </div>`
-        )
-        .openPopup();
-
-      console.log(station.AddressInfo.Latitude);
-    });
   }
 
   initMap(): void {
@@ -135,12 +127,10 @@ export class MapComponent implements OnInit {
             .setView([latitude, longitude], 13)
             .addLayer(baseMapLayer);
 
-          this.fetchPosts();
-
           console.log(this.latitud2);
           console.log(this.longitud2);
           console.log(this.longitud);
-          console.log(this.longitud);
+          console.log(this.latitud);
 
           const customIcon = L.icon({
             iconUrl: '../../../assets/images/iconoCoche.png',
@@ -159,6 +149,7 @@ export class MapComponent implements OnInit {
             const newMarker = L.marker([e.latlng.lat, e.latlng.lng], {
               draggable: true,
             }).addTo(this.map);
+
             this.markers.push(newMarker);
 
             newMarker.on('dragend', (event) => {
@@ -172,24 +163,33 @@ export class MapComponent implements OnInit {
               ],
             })
               .on('routesfound', (e: any) => {
-                const routes = e.routes;
+                const route = e.routes[0];
+                const nextLat =
+                  route.coordinates[route.coordinates.length - 1].lat;
+                const nextLng =
+                  route.coordinates[route.coordinates.length - 1].lng;
 
-                console.log(routes);
-                const name = routes[0].name;
-                this.nombreCalle = name;
+                this.fetchPosts(this.latitud, this.longitud, nextLat, nextLng);
 
-                console.log(name);
-                const distance = routes[0].summary.totalDistance;
-                console.log(distance);
+                this.latitud = nextLat;
+                this.longitud = nextLng;
 
-                e.routes[0].coordinates.forEach((coord: any, index: any) => {
-                  this.routes = routes;
-                  setTimeout(() => {
-                    this.marker.setLatLng([coord.lat, coord.lng], {
-                      draggable: true,
-                    });
-                  }, 100 * index);
-                });
+                // console.log(routes);
+                const name = route.name;
+                this.routes.push(name);
+
+                // console.log(name);
+                // const distance = routes[0].summary.totalDistance;
+                // console.log(distance);
+
+                // e.routes[0].coordinates.forEach((coord: any, index: any) => {
+                //   this.routes = routes;
+                //   setTimeout(() => {
+                //     this.marker.setLatLng([coord.lat, coord.lng], {
+                //       draggable: true,
+                //     });
+                //   }, 100 * index);
+                // });
               })
               .addTo(this.map);
             this.markers.push(newMarker);
