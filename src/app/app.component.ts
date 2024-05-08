697a087a94c62caa8d27fdd4bec07b21cba86271
MichaelSani05 <msmichaelsani@gmail.com>
2024-05-09 00:23:18 +0200

    Implementacion busqueda ubicacion por nombre
    1- Implementada la búsqueda de la ubicación por nombre.
    2- Generación de inputs con el nombre de la ubicación seleccionada cada vez que se seleccione una nueva.
    3- Mostrar el nombre de la ubicación actual en el ASIDE.
    4- Almacenar latitud y longitud de cada una de las ubicaciones que se vayan añadiendo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, LeafletModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/map/aside/aside.component.ts
+++ src/app/map/aside/aside.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MapComponent } from '../map.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { AuthServiceService } from '../../auth-service.service';
-import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [MapComponent, NgFor, NgIf],
+  imports: [MapComponent, NgFor, NgIf, FormsModule],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css',
 })
-export class AsideComponent {
-  constructor(private sharedService: AuthServiceService) {}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AsideComponent implements OnInit{
+  constructor(private sharedService: AuthServiceService, private http: HttpClient, private http2: HttpClient) {}
 
   @Input() longitud: any = '';
   @Input() latitud: any = '';
@@ -20,6 +27,68 @@ export class AsideComponent {
   @Input() latitud2: any = '';
   @Input() nombreCalle: any = '';
   @Input() routes: any = '';
+  query = '';
+  locations: any[] = [];
+  selectedLocation: string = '';
+  isInputDisabled: boolean = false;
+  selectedLocations: any[] = [];
+  nombreUbicacionActual: any;
+  selectedCoordenadas: any[][] = [];
+  coordenadas: any = '';
+ 
+  searchLocation(query: string): void {
+    this.query = query;
+    this.http.get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`)
+    .subscribe((response: any) => {
+        this.locations = response;
+        // Suponiendo que cada ubicación tiene un nombre único, puedes asignar el primer resultado a selectedLocation
+        if (this.locations.length > 0) {
+          this.selectedLocation = this.locations[0].display_name;
+          this.isInputDisabled = true;
+        }
+      }, error => {
+        console.error('Error al buscar ubicaciones:', error);
+      });
+  }
+
+  ngOnInit() {
+    const latitud = 39.4966641;
+    const longitud = -0.4219535;
+
+    this.buscarNombreUbicacion(latitud, longitud);
+
+    console.log(this.selectedCoordenadas);
+  }
+
+  buscarNombreUbicacion(latitud: number, longitud: number): void {
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitud}&lon=${longitud}&format=json`;
+    this.http.get(url).subscribe(
+      (response: any) => {
+        this.nombreUbicacionActual = response.display_name;
+        console.log(this.nombreUbicacionActual);
+      },
+      error => {
+        console.error('Error al buscar la ubicación:', error);
+      }
+    );
+  }
+
+  selectLocation(locationName: string) {
+    this.selectedLocation = locationName;
+    this.isInputDisabled = true;
+    this.query = '';
+    this.selectedLocations.push(locationName);
+    this.http.get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(locationName)}`)
+    .subscribe((response: any) => {
+        console.log(response);
+        this.coordenadas = [response[0].lat, response[0].lon];
+        this.selectedCoordenadas.push(this.coordenadas);
+        console.log(this.selectedCoordenadas);
+      }, error => {
+        console.error('Error al buscar ubicaciones:', error);
+      });
+  }
+
 
   get tipoCoche(): string {
     return this.sharedService.tipoCoche;
