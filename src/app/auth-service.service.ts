31250ee871cf75e6208591895187e8b0be83e768
Carles Sánchez Moreno <119260678+CarlesNero@users.noreply.github.com>
2024-05-10 11:47:40 +0200

    users

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -5,16 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class AuthServiceService {
   private readonly users: { [key: string]: any } = {
-    admin: {
+    Admin: {
       username: 'Admin',
       password: 'admin',
       tipoCoche: 'KIA e-Niro',
     },
-    alfonso: {
+    Alfonso: {
       username: 'Alfonso',
       password: 'admin',
       tipoCoche: 'Peugeot 206',
       casa: '39.52591, -0.41552',
+    },
+    Angel: {
+      username: 'Angel',
+      password: 'admin',
+      tipoCoche: 'Bicicleta',
     },
   };
   public user: any = null;
